fb8666ffa82269186445996aabaa3a66d60c39e1
Lucas3178278745 <muyangzhou8@gmail.com>
2023-02-25 22:21:58 -0800

    passed a & b

--- problem-a/src/components/SenatorTable.js
+++ problem-a/src/components/SenatorTable.js
@@ -1,4 +1,6 @@
 import React from 'react'; //import React library
+import { SenatorRow } from './SenatorRow';
+import { TableHeader } from './TableHeader';
 
 const EXAMPLE_SENATORS = [
   { id: 'C000127', name: 'Maria Cantwell', state: 'WA', party: 'Democrat', phone: '202-224-3441', twitter: 'SenatorCantwell' },
@@ -6,3 +8,15 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
+export function SenatorTable(props) {
+  const { senatorsList } = props;
+  const senatorsElements = senatorsList.map((senatorsList) => {
+    return <SenatorRow key={senatorsList.id} senatorData={senatorsList} />
+  })
+  return (
+    <table className='table table-bordered'>
+      <TableHeader columnNames={['Name', 'State', 'Phone', 'Twitter']} />
+      <tbody>{senatorsElements}</tbody>
+    </table>
+  )
+}
--- problem-a/src/components/TableHeader.js
+++ problem-a/src/components/TableHeader.js
@@ -1,3 +1,16 @@
 import React from 'react'; //import React library
 
 /* Your code goes here */
+export function TableHeader(props) {
+    const { columnNames } = props;
+    const cellElements = columnNames.map((cell) => {
+        return (
+            <th key={cell}>{cell}</th>
+        );
+    });
+    return (
+        <thead>
+            <tr>{cellElements}</tr>
+        </thead>
+    )
+}
